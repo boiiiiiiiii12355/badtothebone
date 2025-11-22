39b76e6d8008c3aca1ddfe01a2ca45f7af95a8f5
tanzettt <taotan429@gmail.com>
2025-11-22 15:33:21 +0800

    action button with animation

--- script/main_ui.gd
+++ script/main_ui.gd
@@ -3,35 +3,36 @@ extends Control
 var button_state : int
 var button_assignment : Array = Array()
 func _ready() -> void:
-	button_assignment = [fightbutton]
-# 0 = fight
-# 1 = act
-# 2 = item
-# 3 = mercy
-func _input(event: InputEvent) -> void:
+	button_assignment = [fightbutton, actbutton, itembutton, mercybutton]
+
+func _physics_process(delta: float) -> void:
 	button_check()
 	button_anim_ctrl(button_state)
+	
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("left"):
-		if button_state <= 3:
-			button_state += 1
+		button_state -= 1
 	elif Input.is_action_just_pressed("right"):
-		if button_state >= 0:
-			button_state -= 1
-
+		button_state += 1
+	clampi(button_state, 0, 3)
+	
 @export var fightbutton : AnimatedSprite2D
+@export var actbutton : AnimatedSprite2D
+@export var itembutton : AnimatedSprite2D
+@export var mercybutton : AnimatedSprite2D
 func button_check():
-	if button_state == 1:
+	if button_state == 0:
 		print("fight")
-	if button_state == 2:
+	if button_state == 1:
 		print("act")
-	if button_state == 3:
+	if button_state == 2:
 		print("item")
-	if button_state == 4:
+	if button_state == 3:
 		print("mercy")
 
 func button_anim_ctrl(button_state):
 	for i in button_assignment.size():
-		var button_sprite : AnimatedSprite2D = button_assignment[button_state]
+		var button_sprite : AnimatedSprite2D = button_assignment[i]
 		if i == button_state:
 			button_sprite.animation = "active"
 		else:
