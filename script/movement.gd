92c5d738945b49b5c8d1643d87a98542d2e9fef9
wongshengjie <shengjie.robotic@gmail.com>
2025-11-22 15:34:36 +0800

    。

--- script/movement.gd
+++ script/movement.gd
@@ -1,19 +1,17 @@
 extends CharacterBody2D
 
-@export var speed = 1000
 var hp = 100
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	var dir = Input.get_vector("left", "right", "up", "down")
+	var speed = 1000
 	velocity = dir * speed
 	move_and_slide()
 
-var hpbar
 @export var hp_bar : ProgressBar
 func hp_mang():
 	hp_bar.value = hp
-	
+
 @export var hit_timer : Timer
 func hit(amount : int):
 	hp -= amount
-	
