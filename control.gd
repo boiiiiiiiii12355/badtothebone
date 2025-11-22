d8220c1864d547902902b84e7847c8c6cd17178a
leeyiqing017 <13175222011lee@gmail.com>
2025-11-22 15:18:57 +0800

    Update control.gd

--- control.gd
+++ control.gd
@@ -2,10 +2,9 @@ extends Control
 
 @export var text : RichTextLabel
 func _physics_process(delta: float) -> void:
-	text.visible_characters += 1
+	if $Timer.is_stopped():
+		$Timer.start()
 
-func _ready():
-	$Timer.start()
 
 func _on_timer_timeout():
 	text.visible_characters += 1
