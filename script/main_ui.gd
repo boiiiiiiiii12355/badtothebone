01bcc5c6959e9121a345aa7deb06a1fd96559c80
Mel_The_Idiot <melvinngoi123@gmail.com>
2025-11-22 15:01:28 +0800

    Update main.tscn, node_2d.tscn, and main_ui.gd

--- script/main_ui.gd
+++ script/main_ui.gd
@@ -1,26 +1,26 @@
 extends Control
 
 var button_state : int
-func _process(delta):
-		pass
-
-# 1 = fight
-# 2 = act
-# 3 = item
-# 4 = mercy
+var button_assignment : Array = Array()
+func _ready() -> void:
+	button_assignment = [fightbutton]
+# 0 = fight
+# 1 = act
+# 2 = item
+# 3 = mercy
 func _input(event: InputEvent) -> void:
 	button_check()
+	button_anim_ctrl(button_state)
 	if Input.is_action_just_pressed("left"):
-		if button_state <= 4:
+		if button_state <= 3:
 			button_state += 1
 	elif Input.is_action_just_pressed("right"):
-		if button_state >= 1:
+		if button_state >= 0:
 			button_state -= 1
 
 @export var fightbutton : AnimatedSprite2D
 func button_check():
 	if button_state == 1:
-		fightbutton.animation = "active"
 		print("fight")
 	if button_state == 2:
 		print("act")
@@ -28,3 +28,11 @@ func button_check():
 		print("item")
 	if button_state == 4:
 		print("mercy")
+
+func button_anim_ctrl(button_state):
+	for i in button_assignment.size():
+		var button_sprite : AnimatedSprite2D = button_assignment[button_state]
+		if i == button_state:
+			button_sprite.animation = "active"
+		else:
+			button_sprite.animation = "default"
